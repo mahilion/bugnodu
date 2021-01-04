0dc081d9fb23c7a5d3e439da0bbaf9e99034850c
mahilion <mahilion01@gmail.com>
2021-01-04 13:32:46 +0530

    fix #3 introduce basic components and navigation

--- frontend/bugtracker/src/app/app-routing.module.ts
+++ frontend/bugtracker/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { ListTicketsComponent } from './list-tickets/list-tickets.component';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component:LoginComponent},
+  {path:'login' , component:LoginComponent},
+  {path:'welcome/:name' , component:WelcomeComponent},
+  {path:'tickets' , component:ListTicketsComponent},
+  {path: '**', component:ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

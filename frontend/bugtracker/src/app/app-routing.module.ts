339e06e6df4c82a5f893b752cd9da718b4bde317
mahilion <mahilion01@gmail.com>
2021-01-06 21:59:39 +0530

    fix #7 add auth service and protect the routes

--- frontend/bugtracker/src/app/app-routing.module.ts
+++ frontend/bugtracker/src/app/app-routing.module.ts
@@ -3,15 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { ErrorComponent } from './error/error.component';
 import { ListTicketsComponent } from './list-tickets/list-tickets.component';
 import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { RouteGuardService } from './service/route-guard.service';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 
 const routes: Routes = [
-  {path: '' , component:LoginComponent},
-  {path:'login' , component:LoginComponent},
-  {path:'welcome/:name' , component:WelcomeComponent},
-  {path:'tickets' , component:ListTicketsComponent},
-  {path: '**', component:ErrorComponent}
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'welcome/:name', component: WelcomeComponent, canActivate: [RouteGuardService] },
+  { path: 'tickets', component: ListTicketsComponent, canActivate: [RouteGuardService] },
+  { path: 'logout', component: LogoutComponent, canActivate: [RouteGuardService] },
+  { path: '**', component: ErrorComponent }
 ];
 
 @NgModule({
